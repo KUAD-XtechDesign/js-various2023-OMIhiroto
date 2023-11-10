cd57d03eabf23890cef39afbc8d25724173fd1ce
OMIhiroto <h-omi6276@st.kyoto-art.ac.jp>
2023-11-10 14:53:37 +0900

    不具合の修正

--- guitar/js/guitar.js
+++ guitar/js/guitar.js
@@ -4,8 +4,8 @@ $(".select_set select").on("change",selectChange)
 let current_code=""
 
 function selectChange(){
-    let val01=$("select01 option:selected").val();
-    let val02=$("select02 option:selected").val();
+    let val01=$("#select01 option:selected").val();
+    let val02=$("#select02 option:selected").val();
     current_code=val01 + val02;
     console.log(current_code)
     $("#code_txt").text(current_code)
